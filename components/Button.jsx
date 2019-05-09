6180bb47084c62eb249dfd19d0e5766a3c10b91f
toshi-toma <toshitoma1217@gmail.com>
2019-05-10 01:42:51 +0900

    install styled-components and add style for button

---
+++ components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+export default Button
