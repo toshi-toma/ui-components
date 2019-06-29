6eb32f84286e4563783f7ff38faaf5c43a0af8b1
toshi-toma <toshitoma1217@gmail.com>
2019-06-29 15:56:58 +0900

    create __stories__ dir  and move file

--- src/components/atoms/__stories__/Button.stories.tsx
+++ src/components/atoms/__stories__/Button.stories.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-import Button from "../components/atoms/Button";
+import Button from "../Button";
 
 storiesOf("Atoms/Button", module).add("with text", () => (
-  <Button onClick={action("clicked")}>Hello Button</Button>
+  <Button onClick={action("clicked")}>Submit</Button>
 ));
